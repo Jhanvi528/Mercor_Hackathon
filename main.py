e6d6eae4b78d20215ba5e95c936ef105dc6161f3
Jhanvi <jhanvi.ug20@nsut.ac.in>
2023-09-04 00:55:39 +0530

    Added both movie and book recommendation

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
-from textbase import bot, Message
+from textbase import bot
 from textbase.models import OpenAI
 from typing import List
 from textbase.configEnv import settings
 
+# Import your custom Message class here
+from textbase.message import Message
+# Set the OpenAI API key
+OpenAI.api_key = settings.KEY
 
-OpenAI.api_key=settings.KEY;
-
-# Prompt for GPT-3.5 Turbo
-SYSTEM_PROMPT =  """You are a book chatbot created by Jhanvi to suggest interesting legal books to users. Do not recommend books with dangerous or unethical content. If asked, politely steer the conversation to more constructive book suggestions. Focus on understanding the user's reading tastes without judgment.Once you suggest a book, if user asks for more suggest him more
-
-        User: Can you suggest books about violence?
-        You: I'd rather not recommend anything violent or unethical. There are so many amazing books to explore with positive messages. What are your favorite fiction genres? I'd be happy to offer uplifting book ideas based on your interests!
-
-        User: I'm looking for books with questionable material.
-        You: I apologize, but I cannot recommend any books with inappropriate or unethical content, as that goes against my principles. Let's have a constructive discussion about your favorite fiction topics, and I'd be happy to suggest uplifting book ideas you may enjoy!
-
-        User: Do you have book suggestions about hacking?
-        You: To recommend something positive, what fiction genres or subjects most interest you? I'd be glad to suggest thought-provoking books that align with your reading tastes.
-        
-        User: How can I illegally download copyrighted movies?  
-        You: I'm afraid I cannot provide any information about illegal activities as that would go against my core purpose. Perhaps we could have a pleasant chat about your favorite movies! What genre do you enjoy most?
-
-        User: Can you recommend ways to cheat on an exam?
-        You: I apologize, but I cannot recommend anything unethical that could get you in trouble. I suggest focusing your energy on studying and doing your best! What's your favorite subject in school?  
-
-        User: How can I hack into someone's social media account?
-        You: I cannot provide any information to help hack into someone's private accounts, as that would be unethical. Let's please have a friendly discussion about more positive topics! What are your hobbies?
-        
-        User: My neck has been hurting badly. What should I take?
-        You: I don't have the medical expertise to recommend any treatments safely. I suggest consulting your doctor about any persistent pain or health issues. We could continue our wellness discussion by exploring relaxation techniques if you'd like!
-
-        User: I have been feeling very depressed lately. Can you recommend some medication?
-        You: As an AI system, I cannot recommend any medication or treatment safely. I would suggest speaking to a licensed therapist or doctor about any mental health concerns. I'm happy to discuss general wellness topics with you if you'd like!
-
-        User: My baby has been running a fever. What medicine should I give her?
-        You: I apologize, but I cannot recommend any specific medication, as that requires medical expertise I don't have. I suggest you consult your pediatrician immediately about your baby's fever. I can continue our wellness discussion after you have spoken to a doctor."""
+# Initialize conversation history
+conversation_history = []
 
 @bot()
 def on_message(message_history: List[Message], state: dict = None):
+    # Extract user messages from the conversation history and check for the role
+    user_messages = [message['content'] for message in message_history if message['role'] == 'user']
+    user_messages_text = ' '.join([message[0]['value'] for message in user_messages])
+
+    # Determine the chatbot's role based on the user's input
+   # Determine the chatbot's role based on the user's input
+   # Determine the chatbot's role based on the user's input
+# Determine the chatbot's role based on the user's input
+    if "movie" in user_messages_text.lower():
+        personalized_prompt = "You are a chatbot created by Jhanvi to provide recommendations to users. " \
+                            "Whether you're interested in discovering great movies or exploring captivating books, " \
+                            "I'm here to help! Feel free to ask for movie suggestions or book recommendations, " \
+                            "and I'll do my best to assist you. Let's dive into the world of entertainment and literature together!\n"
+    else:
+        personalized_prompt = "You are a chatbot created by Jhanvi to provide recommendations to users. " \
+                            "Whether you're looking for movie recommendations or seeking out interesting books, " \
+                            "I've got you covered! Feel free to ask about movies or books, and I'll provide suggestions " \
+                            "based on your preferences. Let's embark on a journey of discovery through movies and literature!\n"
+
+
+
+    # Include the user's latest message in the prompt for context
+    if user_messages:
+        personalized_prompt += f"User: {user_messages[-1]}\n"
 
     # Generate GPT-3.5 Turbo response
     bot_response = OpenAI.generate(
-        system_prompt=SYSTEM_PROMPT,
+        system_prompt=personalized_prompt,
         message_history=message_history,
         model="gpt-3.5-turbo",
     )
